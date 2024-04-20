ef20aff56bc97e402a27cd73463f0f56f0c1c0ce
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-04-20 15:37:04 +0200

    feat: implement basic search functionality

--- static/assets/js/books.js
+++ static/assets/js/books.js
@@ -1,24 +1,42 @@
 const container = document.getElementById('book-container');
-const searchInput = document.getElementById('search-input');
 const loader = document.getElementById('books-loader');
-const searchParams = new URLSearchParams(window.location.search);
+const searchInput = document.getElementById('search-input');
+const searchForm = document.getElementById('search-form');
 
-async function main() {
-  const page = searchParams.get('page');
-  const search = searchParams.get('search');
+renderBooks({
+  page: LibraryState.I.state.page,
+  search: LibraryState.I.state.search,
+});
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const search = searchInput.value.trim();
+
+  const state = { search, page: '1' };
+
+  LibraryState.I.setState(state);
+
+  renderBooks(state);
+});
+
+async function renderBooks({ page, search }) {
+  loader.classList.remove('hidden');
+  container.classList.add('hidden');
 
   if (search?.trim()) {
     searchInput.value = search;
   }
 
-  const books = await fetchBooks({ page, search });
+  const books = await fetchBooks({
+    page,
+    search,
+  });
 
   loader.classList.add('hidden');
+  container.classList.remove('hidden');
 
-  books.forEach((book) => {
-    const bookCard = createBookCard(book);
-    container.appendChild(bookCard);
-  });
+  const bookCards = books.map(createBookCard);
+
+  container.replaceChildren(...bookCards);
 }
-
-main();
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -48,3 +48,51 @@ async function fetchBookById(id) {
 
   return book;
 }
+
+/**
+ * A class to manage the state of the system and synchronize it with the URL.
+ *
+ * The state can only persist strings, using other data types can be unreliable,
+ * so stick to using strings whenever possible.
+ */
+class LibraryState {
+  constructor() {
+    // Set the browser state from the search parameters in the URL.
+    const url = new URL(window.location.href);
+    const state = {};
+
+    url.searchParams.forEach((value, key) => {
+      state[key] = value;
+    });
+
+    window.history.replaceState(state, '', url);
+  }
+
+  static get I() {
+    if (!this._instance) {
+      this._instance = new LibraryState();
+    }
+
+    return this._instance;
+  }
+
+  get state() {
+    return window.history.state;
+  }
+
+  setState(state) {
+    const updatedState = { ...this.state, ...state };
+
+    const url = new URL(window.location.href);
+
+    Object.entries(updatedState).map(([key, value]) => {
+      if (value) {
+        url.searchParams.set(key, value);
+      } else {
+        url.searchParams.delete(key);
+      }
+    });
+
+    window.history.pushState(updatedState, '', url);
+  }
+}
