b4eaf1f24f3d3967561f6894b2eed0d2f6313539
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-04-20 13:18:33 +0200

    feat: support search and add loading indicator

--- static/assets/js/books.js
+++ static/assets/js/books.js
@@ -1,10 +1,19 @@
 const container = document.getElementById('book-container');
+const searchInput = document.getElementById('search-input');
+const loader = document.getElementById('books-loader');
 const searchParams = new URLSearchParams(window.location.search);
 
 async function main() {
   const page = searchParams.get('page');
+  const search = searchParams.get('search');
 
-  const books = await fetchBooks(page);
+  if (search?.trim()) {
+    searchInput.value = search;
+  }
+
+  const books = await fetchBooks({ page, search });
+
+  loader.classList.add('hidden');
 
   books.forEach((book) => {
     const bookCard = createBookCard(book);
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,27 +1,50 @@
+const API_BASE_URL = 'https://freetestapi.com/api/v1/books';
+
 function createBookCard(book) {
   const bookCard = document.createElement('article');
 
   const content = `
-      <img src="${book.cover_image}" alt="${book.title} book cover" />
-      <div class="book-info">
-        <h4>${book.title}</h4>
-        <p>by ${book.author}</p>
-      </div>
-    `;
+    <img src="${book.cover_image}" alt="${book.title} book cover" />
+    <div class="book-info">
+      <h4>${book.title}</h4>
+      <p>by ${book.author}</p>
+    </div>
+  `;
 
   bookCard.innerHTML = content;
 
   return bookCard;
 }
 
-async function fetchBooks(page = 1, limit = 20) {
-  const url = new URL('https://freetestapi.com/api/v1/books');
+async function fetchBooks({ page = 1, limit = 20, search }) {
+  const url = new URL(API_BASE_URL);
+
+  if (isNaN(page)) {
+    page = 1;
+  }
+
+  if (isNaN(limit)) {
+    limit = 20;
+  }
 
   url.searchParams.append('page', page);
   url.searchParams.append('limit', limit);
+
+  if (search?.trim()) {
+    url.searchParams.append('search', search);
+  }
 
   const res = await fetch(url);
   const books = await res.json();
 
   return books;
+}
+
+async function fetchBookById(id) {
+  const url = new URL(`/${id}`, API_BASE_URL);
+
+  const res = await fetch(url);
+  const book = await res.json();
+
+  return book;
 }
