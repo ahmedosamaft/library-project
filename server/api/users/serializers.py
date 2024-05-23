964628acff3f336a71d448ef87877dba18583cbe
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-05-23 22:27:26 +0300

    feat(api): add register endpoint

--- server/api/users/serializers.py
+++ server/api/users/serializers.py
@@ -7,4 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         # Note: the 'username' field is meant to store the user's email as our
         # library system doesn't use usernames but rather emails.
-        fields = ['id', 'username', 'is_staff', 'first_name', 'last_name']
+        fields = ['id', 'username', 'password', 'is_staff', 'first_name', 'last_name']
+        extra_kwargs = { 'password': { 'write_only': True } }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
--- server/api/users/views.py
+++ server/api/users/views.py
@@ -1,6 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 from .serializers import UserSerializer
 
 @api_view(['GET'])
@@ -8,3 +10,21 @@ from .serializers import UserSerializer
 def current_user(request):
     user = UserSerializer(request.user)
     return Response(user.data)
+
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        # Generate an access and refresh token for the new user
+        refresh = RefreshToken.for_user(serializer.instance)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'user': serializer.data
+        }, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
