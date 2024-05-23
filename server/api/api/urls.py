4b5b887256546d3db3788888d2a46e064b3e1d01
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-05-23 19:58:32 +0300

    refactor(api): move token logic to users app

--- server/api/api/urls.py
+++ server/api/api/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/books/', include('books.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
