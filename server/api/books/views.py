7f9abbce1de87531538fdea6c4715cec7b1d40d1
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-05-23 20:53:01 +0300

    feat(api): add permissions to book endpoints

--- server/api/books/views.py
+++ server/api/books/views.py
@@ -1,13 +1,14 @@
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .serializer import *
 from .models import Book, BorrowedList
+from users.permissions import IsAdminOrReadonly
 
-
-# Create your views here.
 
 @api_view(['GET', 'POST'])
+@permission_classes([IsAdminOrReadonly])
 def book_list_create(request):
     if request.method == 'GET':
         books = Book.objects.all()
@@ -25,7 +26,7 @@ def book_list_create(request):
                     genres.append(genre)
                 except Genre.DoesNotExist:
                     return Response({'error': f'Genre with ID {genre_id} does not exist'}, status=status.HTTP_400_BAD_REQUEST)
-                
+
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             book = serializer.save()
@@ -37,7 +38,9 @@ def book_list_create(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['GET', 'PUT','DELETE'])
+@permission_classes([IsAdminOrReadonly])
 def book_detail_update_delete(request, book_id):
     try:
         book = Book.objects.get(pk=book_id)
@@ -61,15 +64,17 @@ def book_detail_update_delete(request, book_id):
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def borrow_book(request, book_id):
     try:
         book = Book.objects.get(pk=int(book_id))
     except Book.DoesNotExist:
         return Response({'error': 'Book does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-    user = User.objects.get(pk=1) # Replace this with the logged in user
-    
+    user = request.user
+
     if request.method == 'POST':
         if book.no_of_copies > 0:
             book.no_of_copies -= 1
@@ -78,23 +83,27 @@ def borrow_book(request, book_id):
             return Response({'message': 'Book borrowed successfully'}, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'No available copies of the book'}, status=status.HTTP_400_BAD_REQUEST)
-       
+
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_borrowed_books(request):
-    user = User.objects.get(pk=1) # Replace this with the logged in user
+    user = request.user
     borrowed_books = BorrowedList.objects.filter(user=user)
     serializer = BorrowedListSerializer(borrowed_books, many=True)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
+@permission_classes([IsAuthenticated])
 def return_book(request, id):
     try:
         borrowed_entry = BorrowedList.objects.get(pk=id)
     except BorrowedList.DoesNotExist:
         return Response({'error': 'Borrowed entry does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-    user = User.objects.get(pk=1) # Replace this with the logged in user
-    
+    user = request.user
+
     if request.method == 'POST':
         if borrowed_entry.user == user:
             borrowed_entry.book.no_of_copies += 1
