eb2aa9e659d6642729193b2997507bc8a4d96cf0
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2024-05-25 00:28:39 +0300

    chore(api): change tokens expiry durations

--- server/api/api/settings.py
+++ server/api/api/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),     
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 # Default primary key field type
